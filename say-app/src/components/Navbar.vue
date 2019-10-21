a3c0673d9908b848ce28e8fc03de3b0a256e904a
Slayde <44580853+DeSlay@users.noreply.github.com>
2019-10-21 14:04:49 +0200

    summary change Navbar

--- say-app/src/components/Navbar.vue
+++ say-app/src/components/Navbar.vue
@@ -1,30 +1,17 @@
 <template>
   <nav>
     <v-toolbar flat app>
-      <v-toolbar-title>SAYFY</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <div>
-        <template v-if="$vuetify.breakpoint.smAndUp">
-        <v-toolbar-items>
-          <v-btn v-for="link in links" :key="link.text" router :to="link.route">{{link.btn}}</v-btn>
-        </v-toolbar-items>
-          
-          <v-btn icon>
-          <v-icon v-for="link in links" :key="link.text" router :to="link.route">{{link.icon}}</v-icon>
-          </v-btn>
-        </template>
-      </div>
-      </v-toolbar>
-
-      <!--      <v-toolbar-items>
-        <v-btn text v-for="link in links" :key="link.text" router :to="link.route">{{link.btn}}</v-btn>
-      </v-toolbar-items>
-      <template v-if="$vuetify.breakpoint.smAndUp">
-        <v-btn icon v-for="link in links" :key="link.text" router :to="link.route">
-          <v-icon>{{link.icon}}</v-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light"></span>
+        <span>SAYFY</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn v-for="link in links" :key="link.text" router :to="link.route">{{link.btn}}</v-btn>
+        <v-btn flat color="grey">
+          <span> Sign Out</span>
+          <v-icon right>exit_to_app</v-icon>
         </v-btn>
-      </template>
-    </v-toolbar>-->
+    </v-toolbar>
   </nav>
 </template>
 
@@ -34,17 +21,6 @@ export default {
     return {
       drawer: false,
       links: [
-        {
-          icon: "mdi-export-variant",
-          text: "mdi-export-variant",
-          route: "/Share"
-        },
-        {
-          icon: "mdi-delete-circle",
-          text: "mdi-delete-circle",
-          route: "/Delete"
-        },
-        { icon: "mdi-plus-circle", text: "mdi-plus-circle", route: "/Ajout" },
         { btn: "Home", text: "click", route: "/Home" },
         { btn: "ABOUT US", text: "click", route: "/About" },
         { btn: "CONTACT", text: "click", route: "/Contact" }
